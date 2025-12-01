93e62d49782b77db8774f4341128036fee2118e1
Brakku <88351016+Brakku@users.noreply.github.com>
2025-12-01 16:15:44 +0000

    coments

--- data/data_wrapper.py
+++ data/data_wrapper.py
@@ -3,34 +3,45 @@ import csv
 
 class DataWrapper:
     def __init__(self):
-        self.playerFilePath:str = "./data/files/filename.txt"
+        self.playerFilePath:str = "./data/files/filename.txt" #the file location
         pass
+    
     
     def write_player(self, new_player:Player) -> bool:
         """ Writes new player data. returns False if failed and True if it worked"""
         
-        with open( self.playerFilePath, 'a' ) as theFile:
-            csvWriter = csv.writer(theFile)
+        with open( self.playerFilePath, 'a' ) as theFile: #open file in append mode
+            
+            csvWriter = csv.writer(theFile) #creates a csv handler
+            
             try:
-                csvWriter.writerow( new_player.toCSVList( ) )
+                csvWriter.writerow( new_player.toCSVList( ) ) # try to write a row
             except:
 				# if we fail, we return false to indicate this.
                 return False
+            
         theFile.close()
-        return True
+        return True # return True
+            
+            
             
     def get_players(self) -> list[Player]:
+        """ opens file and returns a list of players """
         Players = [] # empty list 
         
-        with open( self.playerFilePath, "r+" ) as theFile:
-            csvReader = csv.reader(theFile)
-            next(csvReader) # skip header
-            for line in csvReader:
+        with open( self.playerFilePath, "r+" ) as theFile: # opens file in read mode
+            
+            csvReader = csv.reader(theFile) #creates a csv handler
+            
+            #next(csvReader) # skip header    -- add when file has header
+            
+            for line in csvReader: # reads line by line
+                
 				# turn csv line into player instance
-                player = Player( line[0] ) # more attrebutes as needed
-                Players.append(player) 
+                player = Player( line[0] ) # more attrebutes as needed currently only has name
+                Players.append(player)  #add player to players list
 
         theFile.close()
-        return Players 
+        return Players #return the players
         
         
