502a17f2d3d441156989454b714080b7b82f7ac6
jjing890701-sys <jjing890701@gmail.com>
2025-12-10 14:56:15 +0000

    fix_error_record_games_and_tournaments

--- logic/match_handler.py
+++ logic/match_handler.py
@@ -5,26 +5,28 @@ class match_handler:
     
     def create_match(self, team1_id: int,team2_id: int,tournament_id:int ,date: str, time: str,server_id: int,winner_id: str,Score:int,existing_matches:list[Match]) -> Match | int:
         # 1. Validate Input (Business Logic)        
+
+        if not all(isinstance(x, int) for x in [team1_id, team2_id, tournament_id, server_id, winner_id, Score]):
+            return -2
         
+        if team1_id == team2_id:
+            return -3 
         
-        highest:int = 0
+        try:
+            day, month, year = [int(x) for x in date.split(".")]
+        except ValueError:
+            return -4
+        
+        try:
+            hour, minute = [int(x) for x in time.split(":")]
+        except ValueError:
+            return -5
+
+        highest = 0
         for x in existing_matches:
             if x.id > highest:
                 highest = x.id
-        highest+=1 # find new id
-
-        if not isinstance(team1_id,int) or not isinstance(team2_id,int) or not isinstance(tournament_id,int) or not isinstance(server_id,int) or not isinstance(winner_id,int) or not isinstance(Score,int):
-            return -2
-        
-        string_splitted_at_dots=date.split(".")
-        for i in string_splitted_at_dots:
-            if not isinstance(i,int):
-                return -3
-        
-        string_splitted=time.split(":")
-        for i in string_splitted:
-            if not isinstance(i,int):
-                return -4
+        highest += 1
 
         # 2. Create Model Object
         new_match = Match(highest, team1_id,team2_id,tournament_id,date, time,server_id,winner_id,Score)
