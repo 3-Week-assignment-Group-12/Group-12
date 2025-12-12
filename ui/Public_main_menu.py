88614550ac666c7ac70244bf427122c5fbf3daef
AronBjarkicoding <aronbi2260@gmail.com>
2025-12-12 17:02:40 +0000

    letsgoo

--- ui/Public_main_menu.py
+++ ui/Public_main_menu.py
@@ -358,7 +358,7 @@ View Tournament menu
 1. View All tournaments
 2. View Statistics for tournament
 3. View teams in tournament
-4. View schedule in tournament
+4. View Tournament Results
 b. Back
 
 
@@ -375,7 +375,7 @@ View Tournament menu
 1. View All tournaments
 2. View Statistics for tournament
 3. View teams in tournament
-4. View schedule in tournament
+4. View Tournament Results
 b. Back
 
 Try again!!
